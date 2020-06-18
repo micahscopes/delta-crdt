5bc9f2be0314442cfaa5eb85ed8592c649ea6da0
Micah Fitch <fitchmicah@gmail.com>
2020-06-18 03:44:41 -0500

    reason: added counter delta join test case

--- src/reason-delta-crdts/TestGCounter.bs.js
+++ src/reason-delta-crdts/TestGCounter.bs.js
@@ -15,19 +15,24 @@ Tape.test("test that we can increment the counter", (function (t) {
         return Curry._2(t.ok, undefined, replicaValue === 1);
       }));
 
-Tape.test("test that we can merge two replicas", (function (t) {
+Tape.test("test that we can join deltas/replicas", (function (t) {
         var CounterType = GCounter$DeltaCrdts.Make({
               compare: $$String.compare
             });
         var alice = Curry._1(CounterType.replica, "alice");
         var bob = Curry._1(CounterType.replica, "bob");
+        var marge = Curry._1(CounterType.replica, "marge");
         var match = Curry._1(CounterType.increment, alice);
+        var dAlice = match[1];
         var alice$1 = match[0];
         var match$1 = Curry._1(CounterType.increment, bob);
         var dBob = match$1[1];
         var bob$1 = match$1[0];
-        Curry._2(t.ok, undefined, Curry._1(CounterType.value, Curry._2(CounterType.join, alice$1, dBob)) === Curry._1(CounterType.value, Curry._2(CounterType.join, bob$1, match[1])));
+        Curry._2(t.ok, undefined, Curry._1(CounterType.value, Curry._2(CounterType.join, alice$1, dBob)) === Curry._1(CounterType.value, Curry._2(CounterType.join, bob$1, dAlice)));
         Curry._2(t.ok, undefined, Curry._1(CounterType.value, Curry._2(CounterType.join, alice$1, dBob)) === Curry._1(CounterType.value, Curry._2(CounterType.join, alice$1, bob$1)));
+        var delta = Curry._2(CounterType.join, dAlice, dBob);
+        var marge$1 = Curry._2(CounterType.join, marge, delta);
+        Curry._2(t.ok, undefined, Curry._1(CounterType.value, marge$1) === Curry._1(CounterType.value, Curry._2(CounterType.join, alice$1, bob$1)));
         return Curry._1(t.endTest, undefined);
       }));
 
--- src/reason-delta-crdts/TestGCounter.re
+++ src/reason-delta-crdts/TestGCounter.re
@@ -9,14 +9,23 @@ test("test that we can increment the counter", t => {
   t.ok(replicaValue === 1);
 });
 
-test("test that we can merge two replicas", t => {
+test("test that we can join deltas/replicas", t => {
   module CounterType = GCounter.Make(String);
   open CounterType;
   let alice = CounterType.replica("alice");
   let bob = CounterType.replica("bob");
+  let marge = CounterType.replica("marge");
+
   let (alice, dAlice) = increment(alice);
   let (bob, dBob) = increment(bob);
+  
   t.ok(value(join(alice, dBob)) === value(join(bob, dAlice)));
   t.ok(value(join(alice, dBob)) === value(join(alice, bob)));
+
+  let delta = join(dAlice, dBob);
+  let marge = join(marge, delta);
+
+  t.ok(value(marge) === value(join(alice,bob)));
+
   t.endTest()
 });
